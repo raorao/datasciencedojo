795339b81b77b879dc156797383b464153792c49
Brian Johnson <BJOHN31595@gmail.com>
2016-03-04 12:12:19 -0800

    Added Python TF-IDF module

---
+++ Text Analytics/code/Python-TF-IDF.py
@@ -0,0 +1,48 @@
+"""
+This code is part of Data Science Dojo's bootcamp
+Copyright (C) 2016
+
+Objective: Building a TF-IDF matrix, including stemming for dictionary compression
+Data Source: NLTK sample "movie_reviews" dataset
+Python Version: 3.4+
+Packages: nltk, scikit-learn, scipy, numpy
+"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import find
+from scipy.spatial.distance import cosine
+import numpy as np
+import nltk 
+
+# Import the sample dataset
+from nltk.corpus import movie_reviews
+
+# Description of data set
+print(movie_reviews.readme())
+
+# Prepare document set for stemming
+movie_docs = [' '.join(movie_reviews.words(fileid)) for fileid in movie_reviews.fileids()]
+
+# Define function for tokenizing documents
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    stems = []
+    for item in tokens:
+        item_lower = item.lower()
+        stems.append(nltk.PorterStemmer().stem(item_lower))
+    return stems
+
+# Build TF-IDF matrix
+tfidf = TfidfVectorizer(tokenizer=tokenize)
+movie_tfidf = tfidf.fit_transform(movie_docs)
+
+# Examine non-zero entries in TF-IDF matrix
+find(movie_tfidf[1:6,1:10])
+
+# Calculate pair-wise cosine similarities between first 2000 documents
+movie_cosine = np.empty((2000, 2000))
+for ii in range(0, movie_cosine.shape[0]):
+    for jj in range(ii, movie_cosine.shape[0]):
+        movie_cosine[ii, jj] = cosine(movie_tfidf.getcol(ii).toarray(), movie_tfidf.getcol(jj).toarray())
+        movie_cosine[jj, ii] = movie_cosine[ii, jj]
+
+print(movie_cosine[1:6,1:6])
